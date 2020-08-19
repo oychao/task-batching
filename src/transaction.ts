6e6c0ffca1ecbdf73829b7a21dff0fa95271524c
ouyangchao <ouyangchao@bytedance.com>
2020-08-19 17:14:46 +0800

    feat: async task supported

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,19 +1,19 @@
-import { AnyFunction, Aspect } from './types';
+import { AnyFunction, Aspect, AnyAsyncFunction } from './types';
 
-export abstract class Transaction {
+export abstract class Transaction<T extends AnyFunction = AnyFunction> {
   private aspects: Array<Aspect>;
 
-  private beforeAll: AnyFunction;
+  private beforeAll: T;
 
-  private afterAll: AnyFunction;
+  private afterAll: T;
 
   protected performing: boolean;
 
   constructor() {
     this.performing = false;
     this.aspects = [];
-    this.beforeAll = function () {};
-    this.afterAll = function () {};
+    this.beforeAll = function () {} as T;
+    this.afterAll = function () {} as T;
   }
 
   public install(aspect: Aspect): void {
@@ -24,15 +24,15 @@ export abstract class Transaction {
     this.aspects = [];
   }
 
-  public setBeforeAll(beforeAll: AnyFunction): void {
+  public setBeforeAll(beforeAll: T): void {
     this.beforeAll = beforeAll;
   }
 
-  public setAfterAll(afterAll: AnyFunction): void {
+  public setAfterAll(afterAll: T): void {
     this.afterAll = afterAll;
   }
 
-  public perform<T extends AnyFunction>(fn: T, scope: unknown, ...args: Array<unknown>) {
+  public perform(fn: T, scope: unknown, ...args: Array<unknown>) {
     this.performing = true;
     this.beforeAll();
     const befores = this.aspects.map(({ before }) => before);
@@ -45,6 +45,25 @@ export abstract class Transaction {
       after();
     }
     this.afterAll();
+    this.performing = false;
+  }
+
+  public async performAsync<T extends AnyAsyncFunction>(
+    fn: T,
+    scope: unknown,
+    ...args: Array<unknown>
+  ) {
+    this.beforeAll();
+    const befores = this.aspects.map(({ before }) => before);
+    const afters = this.aspects.map(({ after }) => after);
+    for (const before of befores) {
+      await before();
+    }
+    await fn.apply(scope, args);
+    for (const after of afters) {
+      await after();
+    }
+    await this.afterAll();
     this.performing = false;
   }
 }
